e00e53ebf997039d451145bb40100441cce9b157
Sam Erie <serie@alaska.edu>
2017-09-22 19:42:16 -0800

    fixed class

--- hw2/build.cpp
+++ hw2/build.cpp
@@ -2,5 +2,26 @@
 
 int build(int w, int e, const vector<Bridge> & bridges)
 {
-  return 1;
+  return BridgeBuilder(w, e, bridges).getMaxToll();
+}
+
+//looks up weight of edge, returns -1 if edge doesn't exist
+int BridgeBuilder::getWeight(int w, int e)
+{
+  for(auto bridge : bridges)
+  {
+    if(bridge[0] == w && bridge[1] == e)
+      return bridge[2];
+  }
+  return -1;
+}
+
+BridgeBuilder::BridgeBuilder(int w, int e, const vector<Bridge> & b) : westCount(w), eastCount(e)
+{
+  bridges = b;
+}
+
+int BridgeBuilder::getMaxToll()
+{
+  return 1; //TODO
 }
--- hw2/build.h
+++ hw2/build.h
@@ -5,3 +5,18 @@ using std::vector;
 using Bridge = vector<int>;
 
 int build(int w, int e, const vector<Bridge> & bridges);
+
+class BridgeBuilder
+{
+  vector<Bridge> bridges;
+  int westCount, eastCount;
+  
+  //looks up weight of edge, returns -1 if edge doesn't exist
+  int getWeight(int w, int e);
+
+public:
+  BridgeBuilder(int w, int e, const vector<Bridge> & b);
+
+  int getMaxToll();
+
+};
