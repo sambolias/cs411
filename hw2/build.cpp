73cf1cfc5fe872aafc6fce963f92b526ab21ff48
Sam Erie <serie@alaska.edu>
2017-09-22 18:18:15 -0800

    hw2 skeleton

---
+++ hw2/build.cpp
@@ -0,0 +1,6 @@
+#include "build.h"
+
+int build(int w, int e, const vector<Bridge> & bridges)
+{
+  return 1;
+}
---
+++ hw2/build.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <vector>
+using std::vector;
+using Bridge = vector<int>;
+
+int build(int w, int e, const vector<Bridge> & bridges);
