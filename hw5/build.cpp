6917d704d1bbbe0482a4d4153c45460991de62dd
sambolias <serie@alaska.edu>
2017-11-15 00:39:10 -0900

    passes all tests, refactored, needs comments

--- hw5/build.cpp
+++ hw5/build.cpp
@@ -14,8 +14,6 @@
 
 int build(int w, int e, const vector<Bridge> & bridges)
 {
-  // while(std::cin.get() != 10)
-  // {}
   BridgeBuilder b(w, e, bridges);
   return b.getMaxToll();
 }
@@ -28,19 +26,11 @@ void BridgeBuilder::buildMatrix(const vector<Bridge> & b)
   tollMatrix = vector<vector<int>>(westCities, vector<int>(eastCities, 0));
   for(auto bridge : b)
   {
-    tollMatrix[bridge[0]][bridge[1]] = bridge[2];
-  //  cout<<westCities<<" "<<eastCities<<"\n"<<bridge[0]<<" "<<bridge[1]<<" "<<bridge[2]<<"\n";
+    //in case of multiple tolls defined for a bridge
+    int toll = tollMatrix[bridge[0]][bridge[1]];
+    //store toll in adj mat
+    tollMatrix[bridge[0]][bridge[1]] = max(toll, bridge[2]);
   }
-
-  // for(auto w = 0; w < westCities; w++)
-  // {
-  //   for(auto e = 0; e < eastCities; e++)
-  //   {
-  //       cout<<tollMatrix[w][e]<<" ";
-  //   }
-  //   cout<<"\n";
-  // }
-
 
 }
 
@@ -54,10 +44,7 @@ BridgeBuilder::BridgeBuilder(int w, int e, const vector<Bridge> & b) : westCitie
 
 void BridgeBuilder::calculateMaxToll()
 {
-  //fills empty positions in maxTollMatrix with maximum
-  //toll you could get left or above this matrix position
-  vector<int> maxInRange = vector<int>(westCities, 0);
-  vector<int> maxInDomain = vector<int>(eastCities, 0);
+
   //init matrix with 0s
   maxTollMatrix = vector<vector<int>>(westCities, vector<int>(eastCities, 0));
 
@@ -66,36 +53,19 @@ void BridgeBuilder::calculateMaxToll()
   {
     for(int e = 0; e < eastCities; e++)
     {
-      //if there is a city here
-      if(tollMatrix[w][e] > 0)
-      {
-        int maximum = max(
-                    max((w>0 && e>0) ? maxTollMatrix[w-1][e-1] : 0,
-                    (w-1>0 && e>0) ? maxTollMatrix[w-2][e-1] : 0),
-                    (w>0 && e-1>0) ? maxTollMatrix[w-1][e-2] : 0
-                    );
+        //store maximum previous bridges that can be added to toll
+        int maximum = (w>0 && e>0) ? maxTollMatrix[w-1][e-1] : 0;
+        maximum = max(maximum, (w>0 && e-1>0) ? maxTollMatrix[w-1][e-2] : 0);
+        maximum = max(maximum, (w-1>0 && e>0) ? maxTollMatrix[w-2][e-1] : 0);
         maximum = tollMatrix[w][e] + maximum;
+        //or if row directly above or directly to left is bigger store it
+        maximum = max(maximum, (w>0) ? maxTollMatrix[w-1][e] : 0);
+        maximum = max(maximum, (e>0) ? maxTollMatrix[w][e-1] : 0);
         maxTollMatrix[w][e] = maximum;
-        //gives same output
-      //  maxTollMatrix[w][e] = max(max(maxInRange[w], maxInDomain[e]), maximum);
 
-        maxInRange[w] = max(maxInRange[w], maximum);
-        //maxInRange[w] = max((w>0) ? maxInRange[w-1] : 0, max(maxInRange[w], maximum));
-        maxInDomain[e] = max((e>0) ? maxInDomain[e-1] : 0, max(maxInDomain[e], maximum));
-        // maxInRange[w] = max((w>0) ? maxInRange[w-1] : 0, maximum);
-        // maxInDomain[e] = max((e>0) ? maxInDomain[e-1] : 0, maximum);
         //keep track of overall maximum toll
         maxToll = max(maxToll, maximum);
-      }
-      else
-      {
-      //  maxTollMatrix[w][e] = maxInDomain[e];
-        maxTollMatrix[w][e] = max(maxInRange[w], maxInDomain[e]);
-        // maxTollMatrix[w][e] = max(
-        //                       (w>0) ? maxInRange[w-1] : 0,
-        //                       (e>0) ? maxInDomain[e-1] : 0
-        //                       );
-      }
+
     }
   }
 }
